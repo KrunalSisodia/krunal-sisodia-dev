630f309f4bef43067c9580bb058082de81063505
KrunalSisodia <krunalsisodia@gmail.com>
2020-11-14 11:57:30 +0530

    sidebar CSS work

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import profilePic from '../assets/profilePic.svg'
+import github from '../assets/icons/github.svg'
+import linkedin from '../assets/icons/linkedin.svg'
+import facebook from '../assets/icons/facebook.svg'
+import gmailDark from '../assets/icons/gmail-logo-dark.svg'
+import download from '../assets/icons/direct-download.svg'
 
 const Sidebar =  () =>{
     return(
-        <h1>Sidebar</h1>
+        <div className="sidebarContent">
+            <div className="sidebarDetails">
+                <p className="text-center">
+                    <img src={profilePic} alt="" className="profilePicture" />
+                </p>
+                <p className="profileName">Krunal Sisodia</p>
+                <p className="profileOccupation">UI/UX developer | Web Designer</p>
+            </div>
+            <div className="sidebarContacts">
+                <ul className="sidebarSocialIcons">
+                    <li className="d-inline"><a href="https://github.com/KrunalSisodia"><img src={github} alt="Krunal Sisodia - Github" title="Krunal Sisodia - Github" /></a></li>
+                    <li className="d-inline"><a href="https://www.linkedin.com/in/krunal-sisodia-48ba50124/"><img src={linkedin} alt="Krunal Sisodia - LinkedIn" title="Krunal Sisodia - LinkedIn" /></a></li>
+                    <li className="d-inline"><a href="https://www.facebook.com/krunal.sisodia.1"><img src={facebook} alt="Krunal Sisodia - Facebook" title="Krunal Sisodia - Facebook" /></a></li>
+                    <li className="d-inline"><a href="mailto:krunalsisodia@gmail.com"><img src={gmailDark} alt="Krunal Sisodia - Gmail" title="Krunal Sisodia - Gmail" /></a></li>
+                </ul>
+    <p className="text-center"><a href="" className="profileResume" title="Krunal Sisodia - Resume">Download resume</a></p>
+            </div>
+        </div>
     )
 }
 
