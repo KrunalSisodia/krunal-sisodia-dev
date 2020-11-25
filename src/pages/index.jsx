09c160fd48f57637b665d836489d2b6f2b997c53
KrunalSisodia <krunalsisodia@gmail.com>
2020-11-25 12:09:39 +0530

    optimisation

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,13 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable arrow-spacing */
 /* eslint-disable linebreak-style */
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Layout } from 'antd';
-import ReactGA from 'react-ga';
 import Header from '../components/PageLayout/Header';
 import SidebarWrapper from '../components/PageLayout/Sidebar';
 import AboutMe from '../components/PageFragments/HomePage/AboutMe';
 import Skills from '../components/PageFragments/HomePage/SkillProgress';
 
-useEffect(()=>{
-  ReactGA.initialize('UA-183952095-1');
-
-  // to report page view
-  ReactGA.pageview('/');
-}, []);
 
 export default () => (
   <Layout className="outerPadding">
