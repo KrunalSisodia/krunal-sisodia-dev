0dbda30f36b4cba163a0c5bb5e73c34a73c5ba9e
KrunalSisodia <krunalsisodia@gmail.com>
2020-11-25 12:00:28 +0530

    GA code added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,21 @@
+/* eslint-disable arrow-spacing */
+/* eslint-disable linebreak-style */
 import React, {useEffect} from 'react';
 import { Layout } from 'antd';
+import ReactGA from 'react-ga';
 import Header from '../components/PageLayout/Header';
-
 import SidebarWrapper from '../components/PageLayout/Sidebar';
 import AboutMe from '../components/PageFragments/HomePage/AboutMe';
 import Skills from '../components/PageFragments/HomePage/SkillProgress';
-import ReactGA from 'react-ga';
+
+useEffect(()=>{
+  ReactGA.initialize('UA-183952095-1');
+
+  // to report page view
+  ReactGA.pageview('/');
+}, [])
 
 export default () => (
-  function App (){
-    useEffect(()=>{
-      ReactGA.initialize('UA-183952095-1');
-
-      // to report page view
-      ReactGA.pageview('/');
-    },[])
-  }
   <Layout className="outerPadding">
     <Layout className="container">
       <Header />
