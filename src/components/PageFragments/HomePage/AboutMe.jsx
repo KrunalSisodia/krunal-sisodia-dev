19757e7ce4dd6872a6e9d8f16104dd90d7031efe
KrunalSisodia <krunalsisodia@gmail.com>
2020-11-25 10:52:56 +0530

    GA removed for now

--- src/components/PageFragments/HomePage/AboutMe.jsx
+++ src/components/PageFragments/HomePage/AboutMe.jsx
@@ -5,9 +5,6 @@ import AboutTile from '../../AbouTile';
 import { stripTags, domHtml } from '../../../utils/stripTags';
 import SEO from '../../Seo';
 
-export const initGA = () => {       
-  ReactGA.initialize('G-CPETH0WF9M'); // put your tracking id here
-} 
 
 const pageText = {
   paraOne: `My name is Krunal Sisodia from Vadodara,Gujarat (india). I am a dynamic and multi-disciplined Web designer with a passion for creating innovative design solutions. I produce work that are memorable, meaningful and stand out.`,
