7c657d50dc634137524aecfd9aa6df98dd56c105
KrunalSisodia <krunalsisodia@gmail.com>
2020-12-09 08:56:09 +0530

    instagram link changes

--- src/components/PageLayout/Sidebar/index.jsx
+++ src/components/PageLayout/Sidebar/index.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import {
-  Affix, Layout, Row, Col,
-} from 'antd';
-import FA from 'react-fontawesome';
-import FeatherIcon from 'feather-icons-react';
+/* eslint-disable linebreak-style */
+import React from "react"
+import { Affix, Layout, Row, Col } from "antd"
+import FA from "react-fontawesome"
+import FeatherIcon from "feather-icons-react"
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { globalHistory } from '@reach/router';
-import style from './sidebar.module.less';
-import { useWindowSize } from '../../../utils/hooks';
-import Config from '../../../../config';
+import { globalHistory } from "@reach/router"
+import style from "./sidebar.module.less"
+import { useWindowSize } from "../../../utils/hooks"
+import Config from "../../../../config"
 
-const { Content } = Layout;
+const { Content } = Layout
 const {
-  facebook, github, instagram, twitter, linkedin, behance
-} = Config.social;
+  facebook,
+  github,
+  instagram,
+  twitter,
+  linkedin,
+  behance,
+} = Config.social
 
 const DomContent = () => (
   <aside>
@@ -21,67 +25,100 @@ const DomContent = () => (
     <div className={`${style.name} centerAlign`}>
       <div className={`${style.boxName} centerAlign`}>
         <h2>
-          Krunal
-          {' '}
-          <span>Sisodia</span>
+          Krunal <span>Sisodia</span>
         </h2>
       </div>
       <div className={`${style.badge} ${style.badgeGray}`}>Web Designer</div>
       <div className="centerAlign box">
-        <a href={github} target="_blank" label="button" rel="noopener noreferrer"><FA name="github" /></a>
-        <a href={linkedin} target="_blank" label="button" rel="noopener noreferrer"><FA name="linkedin" /></a>
-        <a href={facebook} target="_blank" label="button" rel="noopener noreferrer"><FA name="facebook-f" /></a>
-        <a href={twitter} target="_blank" label="button" rel="noopener noreferrer"><FA name="twitter" /></a>
-        <a href={behance} target="_blank" label="button" rel="noopener noreferrer"><FA name="behance" /></a>
+        <a
+          href={github}
+          target="_blank"
+          label="button"
+          rel="noopener noreferrer"
+        >
+          <FA name="github" />
+        </a>
+        <a
+          href={linkedin}
+          target="_blank"
+          label="button"
+          rel="noopener noreferrer"
+        >
+          <FA name="linkedin" />
+        </a>
+        <a
+          href={facebook}
+          target="_blank"
+          label="button"
+          rel="noopener noreferrer"
+        >
+          <FA name="facebook-f" />
+        </a>
+        <a
+          href={twitter}
+          target="_blank"
+          label="button"
+          rel="noopener noreferrer"
+        >
+          <FA name="twitter" />
+        </a>
+        <a
+          href={behance}
+          target="_blank"
+          label="button"
+          rel="noopener noreferrer"
+        >
+          <FA name="behance" />
+        </a>
       </div>
       <ul className={`box ${style.badge} contactBlock`}>
         <li className={`${style.contactBlockItem}`}>
           <span>
-            <FeatherIcon size="19" icon="calendar" />
-            {' '}
+            <FeatherIcon size="19" icon="calendar" />{" "}
           </span>
-&nbsp; &nbsp; Aug 22,1995
+          &nbsp; &nbsp; Aug 22,1995
         </li>
         <li className={`${style.contactBlockItem}`}>
-          <span><FeatherIcon size="19" icon="map-pin" /></span>
-          {' '}
-&nbsp; &nbsp; Vadodara, Gujarat, India
+          <span>
+            <FeatherIcon size="19" icon="map-pin" />
+          </span>{" "}
+          &nbsp; &nbsp; Vadodara, Gujarat, India
         </li>
         <li className={`${style.contactBlockItem}`}>
-          <span><FeatherIcon size="19" icon="mail" /></span>
-          {' '}
-&nbsp; &nbsp;
-          <a
-            href="mailto:krunalsisodia@gmail.com"
-            target="_blank"
-          >
+          <span>
+            <FeatherIcon size="19" icon="mail" />
+          </span>{" "}
+          &nbsp; &nbsp;
+          <a href="mailto:krunalsisodia@gmail.com" target="_blank">
             krunalsisodia@gmail.com
           </a>
         </li>
       </ul>
       <div className={style.resumeDownload}>
-        <a href="../resume.pdf" download target="_blank">Download CV</a>
+        <a href="../resume.pdf" download target="_blank">
+          Download CV
+        </a>
       </div>
     </div>
   </aside>
-);
+)
 
-const Sidebar = (props) => {
-  const [width] = useWindowSize();
-  const { children } = props;
-  const { pathname } = globalHistory.location;
-  let domContent = <DomContent />;
+const Sidebar = props => {
+  const [width] = useWindowSize()
+  const { children } = props
+  const { pathname } = globalHistory.location
+  let domContent = <DomContent />
   if (width > 997) {
     domContent = (
       <Affix offsetTop={0}>
         <DomContent />
       </Affix>
-    );
+    )
   }
   if (width < 768) {
-    domContent = <></>;
-    if (pathname === '/') {
-      domContent = <DomContent />;
+    domContent = <></>
+    if (pathname === "/") {
+      domContent = <DomContent />
     }
   }
   return (
@@ -90,35 +127,39 @@ const Sidebar = (props) => {
         <Content className={`${style.content} ${style.background}`}>
           <Row>
             <Col sm={24} md={9} lg={6} className={style.sidebarContent}>
-              { domContent }
+              {domContent}
             </Col>
             <Col sm={24} md={15} lg={18}>
-              <Layout className={`${style.background} ${style.boxContent} borderRadiusSection`}>
-                { children }
+              <Layout
+                className={`${style.background} ${style.boxContent} borderRadiusSection`}
+              >
+                {children}
               </Layout>
             </Col>
           </Row>
         </Content>
       </Layout>
     </>
-  );
-};
+  )
+}
 
-export const Sidebar404 = (props) => {
-  const { children } = props;
+export const Sidebar404 = props => {
+  const { children } = props
   return (
     <Layout>
       <Content className={`${style.content} ${style.background} `}>
         <Row>
           <Col sm={24} md={24} lg={24}>
-            <Layout className={`${style.background} ${style.boxContent} ${style.sideBar404Radius}`}>
+            <Layout
+              className={`${style.background} ${style.boxContent} ${style.sideBar404Radius}`}
+            >
               {children}
             </Layout>
           </Col>
         </Row>
       </Content>
     </Layout>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar
