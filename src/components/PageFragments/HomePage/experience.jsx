83d317619f882b55ca7c739b68f1fb2b2ee847d9
Krunal Sisodia <46866081+KrunalSisodia@users.noreply.github.com>
2020-12-22 08:04:18 +0530

    Update experience.jsx

--- src/components/PageFragments/HomePage/experience.jsx
+++ src/components/PageFragments/HomePage/experience.jsx
@@ -13,49 +13,71 @@ const Experience = () => {
       <div>
 
         <h1 className="titleSeparate">Experience</h1>
-        <p>
-          <strong>Designation:</strong> Web Producer (March 2019 - July 2020)
-        </p>
-        <p>
-          <strong>Company:</strong> Investis digital
-        </p>
-        <p>
-          <strong>Responsibilities:</strong>
-        </p>
-        <ul>
+        <ol>
           <li>
-            Used and contributed to style guides and other design documentation
-            to maintain coherent information and interaction design patterns.
+            <p>
+              <strong>Designation:</strong> Web Producer (March 2019 - July 2020)
+            </p>
+            <p>
+              <strong>Company:</strong> Investis digital
+            </p>
+            <p>
+              <strong>Responsibilities:</strong>
+            </p>
+            <ul>
+              <li>
+                Used and contributed to style guides and other design documentation
+                to maintain coherent information and interaction design patterns.
+              </li>
+              <li>
+                Built user interface, data visualizations and designed overall user
+                experience.
+              </li>
+              <li>
+                Created wireframes, screen-flow diagrams and UI spec documentation.
+              </li>
+              <li>
+                Designed user interface and integrated into content management
+                system to meet client specifications.
+              </li>
+              <li>
+                Identified and eliminated website bugs to improve user experience.
+              </li>
+              <li>
+                Designed user interface to meet client specifications and content
+                management system.
+              </li>
+              <li>
+                Evaluated user research to convert abstract ideas and requirements
+                into planning tools such as journey maps, user personas and
+                storyboards.
+              </li>
+              <li>
+                Devised site maps, user flows, wireframes and mockups for designs
+                based on proven methodologies, stakeholder requests and end-user
+                feedback.
+              </li>
+            </ul>
           </li>
           <li>
-            Built user interface, data visualizations and designed overall user
-            experience.
+            <p>
+              <strong>Designation:</strong> Junior Developer and designer (March 2019 - July 2020)
+            </p>
+            <p>
+              <strong>Company:</strong> The One web technology
+            </p>
+            <p>
+              <strong>Responsibilities:</strong>
+            </p>
+            <ul>
+              <li>Developing front end website architecture</li>
+              <li>Participating in the design and creation of scalable software</li>
+              <li>Work with development teams and product managers to ideate software solutions</li>
+              <li>Ensuring responsiveness of applications</li>
+              <li>Ensuring cross-platform optimization for mobile phones</li>
+            </ul>
           </li>
-          <li>
-            Created wireframes, screen-flow diagrams and UI spec documentation.
-          </li>
-          <li>
-            Designed user interface and integrated into content management
-            system to meet client specifications.
-          </li>
-          <li>
-            Identified and eliminated website bugs to improve user experience.
-          </li>
-          <li>
-            Designed user interface to meet client specifications and content
-            management system.
-          </li>
-          <li>
-            Evaluated user research to convert abstract ideas and requirements
-            into planning tools such as journey maps, user personas and
-            storyboards.
-          </li>
-          <li>
-            Devised site maps, user flows, wireframes and mockups for designs
-            based on proven methodologies, stakeholder requests and end-user
-            feedback.
-          </li>
-        </ul>
+        </ol>
         <hr />
       </div>
     </>
